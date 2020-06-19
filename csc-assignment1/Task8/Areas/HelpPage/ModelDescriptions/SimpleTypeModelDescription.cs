c8b2d25110784c37f7ffccd78ffcfee8851ac26e
WEEKIAT.18 <WEEKIAT.18@ichat.sp.edu.sg>
2020-06-19 16:31:46 +0800

    Task 8

---
+++ csc-assignment1/Task8/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Task8.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ csc-assignment1/Task8/Controllers/MyInfoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Task8.Controllers
+{
+    public class MyInfoController : ApiController
+    {
+        //[HttpGet]
+        //[Route("api/env")]
+        //public IHttpActionResult Env()
+        //{
+
+
+            
+        //}
+    }
+}
